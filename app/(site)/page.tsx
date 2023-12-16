57a65e76775dbc529d565fe427c6e7d9783fad56
Abinash Kar <abinash.kar611@gmail.com>
2023-12-16 12:04:01 +0530

    Final

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 
 import PageContent from "./component/PageContent";
+import Link from "next/link";
 
 export const revalidate = 0;
 
@@ -16,6 +17,7 @@ export default async function Home() {
                <h1 className="text-white text-3xl font-semibold">
                   Welcome back
                </h1>
+               <p>Developed By - <Link className="hover:text-yellow-300" href={"https://abinashkar.com"}>Abinash Kar</Link></p>
                <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
                   <ListItem image="/images/liked.png" name="Liked Songs" href="liked" />
                </div>
